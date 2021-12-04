13e6537c402a900f63f3fb8a3b6770da5fc89f83
Christopher Gallo <cjg100@georgetown.edu>
2021-12-04 18:38:20 -0500

    board built from matrix, board and window correct size

--- Makefile
+++ Makefile
@@ -1,16 +1,8 @@
 
-
-build:
-
-
 run:
-
-
-
-c_run:
-	cd src && gcc life.c `pkg-config --cflags gtk+-3.0 --libs gtk+-3.0` -o life.out
+	cd src && gcc life.c `pkg-config --cflags gtk+-3.0 --libs gtk+-3.0` -o draw.out
 	cd src && ./life.out
 
 
-c_build:
+build:
 	cd src && gcc life.c `pkg-config --cflags gtk+-3.0 --libs gtk+-3.0` -o draw.out
--- src/life.c
+++ src/life.c
@@ -2,24 +2,18 @@
 
 gint main(int argc,char *argv[])
 {
-
-    int row1[] = {1,2,3,4};
-    int row2[] = {4,5,6,7};
-
-    int* mat1[] = {row1, row2};
-    // int mat1[][4] = { {1,2, 3, 4}, {5, 6, 7, 8} };
-    
+    int** mat = init_matrix(MATRIX_HEIGHT, MATRIX_LENGTH);
 
     GtkWidget *window, *drawing_area;
     
     gtk_init (&argc, &argv);
     window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
-    g_signal_connect (window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
+    g_signal_connect (window, "destroy", G_CALLBACK (gtk_main_quit), NULL); // exit function
     
     drawing_area = gtk_drawing_area_new();
     gtk_container_add (GTK_CONTAINER (window), drawing_area);
-    gtk_widget_set_size_request (drawing_area, 300, 500);
-    g_signal_connect (G_OBJECT (drawing_area), "draw", G_CALLBACK (draw_callback), mat1);
+    gtk_widget_set_size_request (drawing_area, 1700, 1000);
+    g_signal_connect (G_OBJECT (drawing_area), "draw", G_CALLBACK (draw_callback), mat); // draw call, spring board for all logic
     gtk_widget_show_all (window);
     gtk_main ();
     return 0;
