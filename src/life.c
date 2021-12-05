f49597875a1e56ea6f42bd7de425c4f563d1c763
Christopher Gallo <cjg100@georgetown.edu>
2021-12-04 21:11:18 -0500

    Game of Life logic impl, but no redraw event impl yet

--- src/life.c
+++ src/life.c
@@ -1,5 +1,34 @@
 #include "../inc/draw_call.h"
 
+gchar buf[256];
+
+gboolean on_expose_event(GtkWidget *widget,
+    GdkEventExpose *event,
+    gpointer data) {
+
+    // draw_callback(widget, );
+
+  return FALSE;
+}
+
+gboolean time_handler(GtkWidget *widget) {
+    
+//   if (widget->window == NULL) return FALSE;
+
+  GDateTime *now = g_date_time_new_now_local(); 
+  gchar *my_time = g_date_time_format(now, "%H:%M:%S");
+  
+//   g_sprintf(buf, "%s", my_time);
+  
+  g_free(my_time);
+  g_date_time_unref(now);
+
+  gtk_widget_queue_draw(widget);
+  
+  return TRUE;
+}
+
+
 gint main(int argc,char *argv[])
 {
     int** mat = init_matrix(MATRIX_HEIGHT, MATRIX_LENGTH);
@@ -15,6 +44,7 @@ gint main(int argc,char *argv[])
     gtk_widget_set_size_request (drawing_area, 1700, 1000);
     g_signal_connect (G_OBJECT (drawing_area), "draw", G_CALLBACK (draw_callback), mat); // draw call, spring board for all logic
     gtk_widget_show_all (window);
+    // g_timeout_add(1000, (GSourceFunc) draw_callback, (gpointer) window);
     gtk_main ();
     return 0;
 }
