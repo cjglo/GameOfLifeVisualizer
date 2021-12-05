b8d2aa89f2862fbbd2842b8f678106a5432c60ab
Christopher Gallo <cjg100@georgetown.edu>
2021-12-04 22:49:54 -0500

    Finished Alpha version of Game of Life!

--- src/life.c
+++ src/life.c
@@ -2,31 +2,12 @@
 
 gchar buf[256];
 
-gboolean on_expose_event(GtkWidget *widget,
-    GdkEventExpose *event,
-    gpointer data) {
-
-    // draw_callback(widget, );
-
-  return FALSE;
+gboolean timeout(GtkWidget *widget)
+{
+    gtk_widget_queue_draw(widget);
+    return TRUE;
 }
 
-gboolean time_handler(GtkWidget *widget) {
-    
-//   if (widget->window == NULL) return FALSE;
-
-  GDateTime *now = g_date_time_new_now_local(); 
-  gchar *my_time = g_date_time_format(now, "%H:%M:%S");
-  
-//   g_sprintf(buf, "%s", my_time);
-  
-  g_free(my_time);
-  g_date_time_unref(now);
-
-  gtk_widget_queue_draw(widget);
-  
-  return TRUE;
-}
 
 
 gint main(int argc,char *argv[])
@@ -44,7 +25,7 @@ gint main(int argc,char *argv[])
     gtk_widget_set_size_request (drawing_area, 1700, 1000);
     g_signal_connect (G_OBJECT (drawing_area), "draw", G_CALLBACK (draw_callback), mat); // draw call, spring board for all logic
     gtk_widget_show_all (window);
-    // g_timeout_add(1000, (GSourceFunc) draw_callback, (gpointer) window);
+    g_timeout_add(1000, (GSourceFunc)timeout, window);
     gtk_main ();
     return 0;
 }
